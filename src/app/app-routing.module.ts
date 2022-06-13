dc03cd1c376025d4af033da04762718a6f834e74
mateus-ceccagno <sk.mateus@hotmail.com>
2022-06-12 22:21:50 -0300

    init registration account component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './user/login/login.component';
+import { RegistrationComponent } from './user/registration/registration.component';
 import { UserComponent } from './user/user.component';
 
 const routes: Routes = [
   {
     path: 'user',
-    //component: UserComponent,
+    // component: UserComponent,
     children: [
       { path: 'login', component: LoginComponent },
-      //{ path: 'registration', component: RegistrationComponent },
+      { path: 'registration', component: RegistrationComponent },
     ],
   },
 ];
